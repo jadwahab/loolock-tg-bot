30ef0d14ab55a9c8ee0bf9f8d5addca1e84437fa
Jad Wahab <15110087+jadwahab@users.noreply.github.com>
2023-10-11 01:30:25 +0300

    kick but don't ban

--- helpers/kickuser.go
+++ helpers/kickuser.go
@@ -27,14 +27,26 @@ func KickUser(bot *tgbotapi.BotAPI, ka *KickArgs) {
 	})
 	if err != nil {
 		log.Printf("Failed to kick user: %s", err)
-	} else {
-		_, err = bot.Send(tgbotapi.NewMessage(ka.ChatID, fmt.Sprintf("@%s has just been kicked...", ka.UserName)))
-		if err != nil {
-			log.Printf("Failed to send message: %s", err)
-		}
-		err = ka.DBP.RemoveUserFromGroupChatDB(ka.ChatID, ka.UserID)
-		if err != nil {
-			log.Printf("Failed to remove user from group chat DB: %s", err)
-		}
+		return
+	}
+
+	// Immediately unban the user after kicking
+	_, err = bot.Request(tgbotapi.UnbanChatMemberConfig{
+		ChatMemberConfig: tgbotapi.ChatMemberConfig{
+			ChatID: ka.ChatID,
+			UserID: ka.UserID,
+		},
+	})
+	if err != nil {
+		log.Printf("Failed to unban user: %s", err)
+	}
+
+	_, err = bot.Send(tgbotapi.NewMessage(ka.ChatID, fmt.Sprintf("@%s has just been kicked...", ka.UserName)))
+	if err != nil {
+		log.Printf("Failed to send message: %s", err)
+	}
+	err = ka.DBP.RemoveUserFromGroupChatDB(ka.ChatID, ka.UserID)
+	if err != nil {
+		log.Printf("Failed to remove user from group chat DB: %s", err)
 	}
 }
